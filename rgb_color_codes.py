1c959c25ed61f26fb5dbb6c4830505afd77a2981
Eddy GASANA <e.gasana@alustudent.com>
2023-10-04 13:38:59 +0200

    Update rgb_color_codes.py

--- rgb_color_codes.py
+++ rgb_color_codes.py
@@ -7,7 +7,7 @@ pattern = r'rgb\([0-255],\s[0-255],\s[0-255])'
 api_response = 
 """
 set of rgb color codes:
-rgb(29, 56, 129), rgb(61, 220, 220), rgb(144, 34, 183), rgb(243, 145, 7), rgb(245, 42, 42)
+rgb(29, 56, 129), rgb(61, 220, 220), rgb(144, 34, 183), rgb(243, 145, 7), rgb(245, 42, 42), rgb(98, 255, 300)
 """
 
 rgb_color_codes = re.findall(pattern, api_response)
